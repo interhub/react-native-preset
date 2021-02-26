5620e709a5a517d8cadbf024e911e771e24840a5
stepanturchenko <stepanstepan4@gmail.com>
2021-02-26 17:11:55 +0200

    add use code push update new pattern

--- src/hook/load_config/useCodePush.ts
+++ src/hook/load_config/useCodePush.ts
@@ -1,32 +1,36 @@
 import * as SplashScreen from 'expo-splash-screen';
 import codePush from 'react-native-code-push'
 
-/** 
-TIME BEFORE RESET APP START (AFTER UPDATE)
-*/
+/**
+ TIME BEFORE RESET APP START (AFTER UPDATE)
+ */
 const SLEEP_TIME = 1000
 
 
-/**  
-@hook for code push reload and update
-*/
+/**
+ @hook for code push reload and update
+ */
 const useCodePush = () => {
-	const syncCodePush = async (): Promise<boolean> => {
-		return new Promise((ok) => {
-			codePush.sync({ installMode: codePush.InstallMode.IMMEDIATE, },
-				(status) => {
-					switch (status) {
-						case codePush.SyncStatus.UP_TO_DATE: SplashScreen.hideAsync(); return ok(true)
-						case codePush.SyncStatus.UNKNOWN_ERROR: return ok(true)
-					}
-				},
-				async (progress) => {
-					if (progress.receivedBytes === progress.totalBytes) {
-						setTimeout(ok, SLEEP_TIME, true)
-					}
-				})
-		})
-	}
-	return { syncCodePush }
+    const syncCodePush = async (): Promise<boolean> => {
+        if (__DEV__) return false
+        return new Promise((ok) => {
+            codePush.sync({installMode: codePush.InstallMode.IMMEDIATE,},
+                (status) => {
+                    switch (status) {
+                        case codePush.SyncStatus.UP_TO_DATE:
+                            SplashScreen.hideAsync();
+                            return ok(true)
+                        case codePush.SyncStatus.UNKNOWN_ERROR:
+                            return ok(true)
+                    }
+                },
+                async (progress) => {
+                    if (progress.receivedBytes === progress.totalBytes) {
+                        setTimeout(ok, SLEEP_TIME, true)
+                    }
+                })
+        })
+    }
+    return {syncCodePush}
 }
 export default useCodePush
