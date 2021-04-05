010d846c08ead8b12dedbfb3a0dee61a63deb4e3
stepanturchenko <stepanstepan4@gmail.com>
2021-04-05 16:42:56 +0200

    add text shadow to get color module

--- src/config/getShadow.ts
+++ src/config/getShadow.ts
@@ -1,11 +1,15 @@
 /**
-get shadow state for layout style
-*/
-export default function getShadow(val = 1): Object {
+ get shadow state for layout style
+ */
+export default function getShadow(size = 1, shadowColor = '#000000') {
   return {
-    elevation: val * 2,
-    shadowColor: '#000',
-    shadowOpacity: val / 5,
+    elevation: size * 2,
+    shadowRadius: size * 2,
+    shadowColor,
+    shadowOpacity: size / 5,
     shadowOffset: {height: 1, width: 0},
-  }
+    textShadowColor: shadowColor,
+    textShadowRadius: 0,
+    textShadowOffset: {width: 2, height: 2},
+  };
 }
