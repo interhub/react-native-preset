f9993dddafecdaef5fc0ad4282f9a486244ccc6c
stepanturchenko <stepanstepan4@gmail.com>
2021-06-28 15:41:40 +0200

    add doc animate hook nav

--- src/hook/layout/useNavigationAnimateValue.ts
+++ src/hook/layout/useNavigationAnimateValue.ts
@@ -3,6 +3,9 @@ import {useCardAnimation} from '@react-navigation/stack'
 
 type animateNavConfigType = { startOpen: number, endOpen: number, extra?: Animated.ExtrapolateType }
 
+/**
+ get animation context from react navigation animation and use it from animated interpolate low
+ */
 const useNavigationAnimateValue = ({startOpen, endOpen, extra}: animateNavConfigType) => {
     const {current: {progress}} = useCardAnimation()
     return progress.interpolate({inputRange: [0, 1], outputRange: [startOpen, endOpen], extrapolate: extra})
